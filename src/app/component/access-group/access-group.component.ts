fc2158aa02e91cf899d704fa1db4a94d40540ffc
klluv <andhikaka05@gmail.com>
2023-12-15 11:16:37 +0700

    add role added

--- src/app/component/access-group/access-group.component.ts
+++ src/app/component/access-group/access-group.component.ts
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { CookieService } from 'ngx-cookie-service';
 import Swal from 'sweetalert2';
 
+declare var $: any;
+
 interface Role {
   role_uuid: string;
   role_order: number;
@@ -22,6 +24,9 @@ interface Role {
   styleUrls: ['./access-group.component.scss']
 })
 export class AccessGroupComponent implements OnInit {
+
+  role_code: string = '';
+  role_title: string = '';
 
   constructor(private cookieService: CookieService) {}
 
@@ -44,7 +49,54 @@ export class AccessGroupComponent implements OnInit {
     })
   }
 
-  addRole() {
-    
+  addRoleModal() {
+    $('#addRoleModal').modal('show');
+  }
+
+  addApplication() {
+    const token = this.cookieService.get('userToken');
+
+    axios.post(`http://localhost:8080/superadmin/role/add`,
+    { role_code: this.role_code, role_title: this.role_title }, 
+    {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then((response) => {
+      console.log(response.data.message);
+      Swal.fire({
+        title: 'Success',
+        text: response.data.message,
+        icon: 'success'
+      });
+    })
+    .catch((error) => {
+      if(error.response.status === 400) {
+        Swal.fire({
+          title: 'Error',
+          text: error.response.data.message,
+          icon: 'error'
+        });
+      } else if(error.response.status === 422) {
+        Swal.fire({
+          title: 'Error',
+          text: error.response.data.message,
+          icon: 'error'
+        });
+      } else if (error.response.status === 500) {
+        Swal.fire({
+          title: 'Error',
+          text: error.response.data.message,
+          icon: 'error'
+        });
+      } else {
+        Swal.fire({
+          title: 'Error',
+          text: 'Terjadi kesalahan',
+          icon: 'error'
+        });
+      }
+    });
   }
 }
